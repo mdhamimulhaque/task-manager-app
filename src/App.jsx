efe3956ff59c6bb00023308719e2e0b9452e61dd
md Hamimul Haue <mdhamimulhaque@gmail.com>
2024-01-15 17:54:13 +0600

    add to task model added and task handle created [done]

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,28 @@ import { useState } from "react";
 import SearchBox from "./components/SearchBox";
 import TaskActions from "./components/Task/TaskActions";
 import TaskCard from "./components/Task/TaskCard";
+import TaskModal from "./components/Task/TaskModal";
 import { defaultTaskData } from "./data/defaultTaskData";
 
 const TaskBoard = () => {
   const [tasks, setTasks] = useState([defaultTaskData]);
 
+  function handleAddTask() {
+    console.log("clicked");
+  }
+
   return (
-    <main className="p-4 bg-gray-700 h-full ">
+    <main className="p-4 bg-gray-700 h-full  ">
+      <section className="bg-gray-700 fixed z-20 top-0 left-0 w-full h-full">
+        <TaskModal />
+      </section>
       <div className="container mx-auto border-2 border-gray-400 h-screen px-4 pb-4">
         <h2 className="text-4xl py-3 px-2 text-center text-white my-4 text-semibold">
           Task manager app
         </h2>
+
         <SearchBox />
-        <TaskActions />
+        <TaskActions onAddTask={handleAddTask} />
         {tasks?.map((task) => (
           <TaskCard key={task.id} task={task} />
         ))}
---
+++ src/components/Task/TaskModal.jsx
@@ -0,0 +1,78 @@
+const TaskModal = () => {
+  return (
+    <form className="mx-auto w-full max-w-[740px] rounded-xl border border-gray-400 bg-[#191D26] p-9 max-md:px-4 lg:my-10 lg:p-12">
+      <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
+        Add New Task
+      </h2>
+
+      <div className="space-y-5 text-white">
+        <div className="space-y-2 lg:space-y-3">
+          <label htmlFor="title">Title</label>
+          <input
+            className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
+            type="text"
+            name="title"
+            id="title"
+            required
+          />
+        </div>
+
+        <div className="space-y-2 lg:space-y-3">
+          <label htmlFor="description">Description</label>
+          <textarea
+            className="block min-h-[120px] w-full rounded-md bg-[#2D323F] px-3 py-2.5 lg:min-h-[180px]"
+            type="text"
+            name="description"
+            id="description"
+            required
+          ></textarea>
+        </div>
+
+        <div className="grid-cols-2 gap-x-4 max-md:space-y-9 md:grid lg:gap-x-10 xl:gap-x-20">
+          <div className="space-y-2 lg:space-y-3">
+            <label htmlFor="tags">Tags</label>
+            <input
+              className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
+              type="text"
+              name="tags"
+              id="tags"
+              required
+            />
+          </div>
+
+          <div className="space-y-2 lg:space-y-3">
+            <label htmlFor="priority">Priority</label>
+            <select
+              className="block w-full cursor-pointer rounded-md bg-[#2D323F] px-3 py-2.5"
+              name="priority"
+              id="priority"
+              required
+            >
+              <option value="">Select Priority</option>
+              <option value="low">Low</option>
+              <option value="medium">Medium</option>
+              <option value="high">High</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-8 flex justify-between">
+        <button
+          type="submit"
+          className="rounded bg-red-600 px-4 py-2 text-white transition-all hover:bg-red-500"
+        >
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:bg-blue-500"
+        >
+          Create new Task
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default TaskModal;
