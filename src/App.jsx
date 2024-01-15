497e4af6d4ab3e2d23bf88e88fa8affb1dcd7414
md Hamimul Haue <mdhamimulhaque@gmail.com>
2024-01-15 16:35:04 +0600

    task manager ui basic [done]

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import SearchBox from "./conponents/SearchBox";
+import TaskActions from "./conponents/Task/TaskActions";
+import TaskCard from "./conponents/Task/TaskCard";
+
 const App = () => {
   return (
-    <div>
-      <h2 className="text-2xl text-red-300">Task manager app</h2>
-    </div>
+    <main className="p-4 bg-gray-700 h-full ">
+      <div className="container mx-auto border-2 border-gray-400 h-screen px-4 pb-4">
+        <h2 className="text-4xl py-3 px-2 text-center text-white my-4 text-semibold">
+          Task manager app
+        </h2>
+        <SearchBox />
+        <TaskActions />
+        <TaskCard />
+      </div>
+    </main>
   );
 };
 
---
+++ src/conponents/Task/TaskActions.jsx
@@ -0,0 +1,24 @@
+import { FaCirclePlus, FaPrescriptionBottleMedical } from "react-icons/fa6";
+
+const TaskActions = () => {
+  return (
+    <section className="flex gap-4 justify-end">
+      <button
+        type="button"
+        className="relative inline-flex items-center gap-1 px-3.5 py-2 rounded border border-transparent bg-blue-600 hover:bg-blue-500 duration-300 text-sm text-white font-medium"
+      >
+        <FaCirclePlus />
+        <span>Add Task</span>
+      </button>
+      <button
+        type="button"
+        className="relative inline-flex items-center gap-1 px-3.5 py-2 rounded border border-transparent bg-red-600 hover:bg-red-500 duration-300 text-sm text-white font-medium"
+      >
+        <FaPrescriptionBottleMedical />
+        <span>Delete All</span>
+      </button>
+    </section>
+  );
+};
+
+export default TaskActions;
